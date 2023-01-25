ca27370203aed21c324bf748fca687da2deb1cdd
Ali-Gul <minddeveloper936@gmail.com>
2023-01-25 15:33:22 +0400

    AddContainers

---
+++ Live Score/Cell views/TitleCell/TitleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TitleTableViewCell.swift
+//  Live Score
+//
+//  Created by Qoo on 25/01/2023.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
