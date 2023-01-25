9df2d44fd6d96a2a935e3de291e9787eb245d3d0
Mohammad <mohammadkhadeer1992@gmail.com>
2023-01-25 15:50:58 +0400

    Start to make main list part LV ----->  live matches MT ---->  Match Today FN ---->  Featured News

---
+++ Live Score/View/Cells/TableViewCells/FeaturedNews /FeaturedNewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeaturedNewsTableViewCell.swift
+//  Live Score
+//
+//  Created by KHADER on 25/01/2023.
+//
+
+import UIKit
+
+class FeaturedNewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
